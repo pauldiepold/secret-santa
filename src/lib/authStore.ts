255953af5cf5eba6a10e69a9ccc03dd07ece996c
Paul Diepold <paul.diepold@yahoo.de>
2024-10-11 18:25:55 +0200

    fix envs

--- src/lib/authStore.ts
+++ src/lib/authStore.ts
@@ -3,13 +3,6 @@ import type { User } from '@supabase/supabase-js';
 import { supabase } from '$lib/supabaseClient';
 import { goto } from '$app/navigation';
 
-const REDIRECT_URL =
-	import.meta.env.VITE_VERCEL == 1 ? import.meta.env.VITE_VERCEL_URL : 'http://localhost:5173';
-
-console.log(REDIRECT_URL);
-console.log(import.meta.env.VITE_VERCEL);
-console.log(import.meta.env.VITE_VERCEL_URL);
-
 function createAuthStore() {
 	const { subscribe, set } = writable<User | null>(null);
 
@@ -18,9 +11,6 @@ function createAuthStore() {
 		signIn: () =>
 			supabase.auth.signInWithOAuth({
 				provider: 'google',
-				options: {
-					redirectTo: REDIRECT_URL
-				}
 			}),
 		signOut: async () => {
 			await supabase.auth.signOut();
