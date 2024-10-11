0c1e632270ac41bc340cbe75646314449a00cbe7
Paul Diepold <paul.diepold@yahoo.de>
2024-10-11 18:07:59 +0200

    fix envs

--- src/lib/authStore.ts
+++ src/lib/authStore.ts
@@ -4,9 +4,11 @@ import { supabase } from '$lib/supabaseClient';
 import { goto } from '$app/navigation';
 
 const REDIRECT_URL =
-	import.meta.env.VITE_VERCEL === '1' ? import.meta.env.VITE_VERCEL_URL : 'http://localhost:5173';
+	import.meta.env.VITE_VERCEL == 1 ? import.meta.env.VITE_VERCEL_URL : 'http://localhost:5173';
 
 console.log(REDIRECT_URL);
+console.log(import.meta.env.VITE_VERCEL);
+console.log(import.meta.env.VITE_VERCEL_URL);
 
 function createAuthStore() {
 	const { subscribe, set } = writable<User | null>(null);
