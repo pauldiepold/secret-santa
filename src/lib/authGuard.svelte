598cb81cb6fccb3608d28476935cfe2399c73895
Paul Diepold <paul.diepold@yahoo.de>
2024-10-12 19:43:13 +0200

    fixing oauth on vercel

--- src/lib/authGuard.svelte
+++ src/lib/authGuard.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { authStore } from '$lib/authStore';
+	import { userStore } from '$lib/userStore';
 
 	export let redirectTo = '/';
 
 	onMount(async () => {
-		await authStore.check();
-		if (!$authStore) {
+		await userStore.check();
+		if (!$userStore) {
 			goto(redirectTo);
 		}
 	});
 </script>
 
-{#if $authStore}
+{#if $userStore}
 	<slot />
 {/if}
